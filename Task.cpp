23fe4f76c912505511d8c17bed6343de534a4af3
hucha21 <hhafuric@gmail.com>
2023-05-11 09:11:52 +0200

    Added comments to the code, added README file

--- Task.cpp
+++ Task.cpp
@@ -1,24 +1,11 @@
 ﻿// Task.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// Task description:
-// You are given an array A consisting of N integers and an integer X. 
-// Your goal is to have as many equal integers as possible in the array.To achieve this goal, you can do the following operation :
-// Choose an index i(1≤i≤N) and set Ai = Ai⊕X, where ⊕ denotes the bitwise xor operation.
-// Find the maximum number of equal integers you can have in the final array and the minimum
-// number of operations to obtain these many equal integers.
-// CONSTRAINTS:
-// 1≤T≤10 ^ 4
-// 1≤N≤10 ^ 5
-// 0≤X≤10 ^ 9
-// 0≤Ai≤10 ^ 9
-// The sum of N over all test cases does not exceed 5⋅10 ^ 5.
-
-struct rezultat {
+struct rezultat { //struktura za memorisanje rezultata
     int rez, br_operacija;
 };
 #include <iostream>
 #include <unordered_map>
 using namespace std;
-typedef long long ll;
+typedef long long ll;// long long zbog uslova zadatka
 int main()
 {
     int T, n = 0;
@@ -26,14 +13,14 @@ int main()
     rezultat final;
     cin >> T; //unos broja testnih slučajeva
     while (n<T) { //unos testnih slučajeva
-        ll N, X, a;
+        ll N, X; //N->broj elemenata u nizu;X->operator za XOR;
         unordered_map<ll, ll> niz,br_oper;
         cin >> N >> X;
 
         vector<ll>v(N);
         for (ll i = 0; i < N; i++) {
             cin >> v[i];
-            niz[v[i]]++; //broj pjavljivanja
+            niz[v[i]]++; //broj pojavljivanja
             br_oper[v[i] ^ X]++; //broj operacija da se dobije vrijednost
         } 
         ll ans = 0,promjene = 1e18;
@@ -48,7 +35,7 @@ int main()
             promjene = 0;
         }
         else {
-            for (ll i = 0; i < N; i++) {
+            for (ll i = 0; i < N; i++) { //trazimo kombinaciju najveci broj ponavljanja + najmanji broj operacija
                 if (niz[v[i]] + br_oper[v[i]] > ans) {
                     ans = niz[v[i]] + br_oper[v[i]];
                     promjene = br_oper[v[i]];
@@ -58,14 +45,14 @@ int main()
                 }
             }
         }
-        final.rez = ans; final.br_operacija = promjene; //upis rezultata u vektor za upis
-        rez.push_back(final);
+        final.rez = ans; final.br_operacija = promjene; 
+        rez.push_back(final);//upis rezultata u vektor za upis
         n++;
     }
-    //ispis rezultata
-    for(int i=0;i<rez.size();i++)
+    for(int i=0;i<rez.size();i++) //ispis rezultata
     {
         cout << rez[i].rez << " " << rez[i].br_operacija << endl;
     }
+    system("Pause");
     return 0;
 }
